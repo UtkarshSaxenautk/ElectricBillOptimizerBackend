b0dd70994eecea51868b6d0ef5bc34759b74632b
Utkarsh Saxena <nive54@gmail.com>
2022-09-09 12:30:24 +0530

    schema modification

--- models/index.js
+++ models/index.js
@@ -3,21 +3,51 @@ import mongoose, { Schema } from 'mongoose';
 Schema = new mongoose.Schema
 
 const AppliancesSchema = new Schema({
-    total : {
-        type: 'integer',
+    user : {
+        type: 'string',
         required: true,
     },
-    applainces: [
+    total: {
+        type: 'number',
+        required:true,
+    },
+    appliances: [
         {
             name: {
                 type: 'string',
                 required: true,
             },
-            consumption: {
-                type: 'integer',
+            power: {
+                type: 'number',
                 required: true,
+            },
+            expectedhour: {
+                type: 'number',
+                required: true,
+            },
+            location: {
+                type: 'string',
+                required: true
             }
-        }
+        },
+        {
+            name: {
+                type: 'string',
+                required: true,
+            },
+            power: {
+                type: 'number',
+                required: true,
+            },
+            expectedhour: {
+                type: 'number',
+                required: true,
+            },
+            location: {
+                type: 'string',
+                required: true
+            }
+        },
     ]
 })
 
