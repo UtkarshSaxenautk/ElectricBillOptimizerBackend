50d8567332dc2d871e93a4a0d2cc037f6b1a3a2c
Utkarsh Saxena <nive54@gmail.com>
2022-09-17 15:27:17 +0530

    modified call

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const controller =  require('../controller/index.js');
 
 const router = express.Router();
 router.get('/:user_id', controller.readUser);
-router.post('/', controller.createUser);
+router.post('/createUser', controller.createUser);
 router.get('/appliance/:name', controller.readAppliance);
 router.post('/appliance', controller.writeAppliance);
 router.post('/hour', controller.sendHourlyReport)
